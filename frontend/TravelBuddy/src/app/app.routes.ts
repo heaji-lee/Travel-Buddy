c9fa857363e875688cd7ff60595680fe4f9af57a
Heaji Lee <helenheajilee@gmail.com>
2026-01-28 19:36:08 +0000

    added manage component

--- frontend/TravelBuddy/src/app/app.routes.ts
+++ frontend/TravelBuddy/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './modules/home/pages/home';
 import { NewTrip } from './modules/new-trip/pages/new-trip';
 import { AllTrips } from './modules/all-trips/pages/all-trips';
+import { Manage } from './modules/manage/manage';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'home', component: Home },
     { path: 'new-trip', component: NewTrip },
     { path: 'all-trips', component: AllTrips },
+    { path: 'manage', component: Manage },
 ];
--- frontend/TravelBuddy/src/app/modules/new-trip/pages/new-trip.ts
+++ frontend/TravelBuddy/src/app/modules/new-trip/pages/new-trip.ts
@@ -3,10 +3,12 @@ import { FormsModule } from "@angular/forms";
 import { InputTextModule } from 'primeng/inputtext';
 import { AccordionModule } from 'primeng/accordion';
 import { Button } from "primeng/button";
+import { DatePickerModule } from 'primeng/datepicker';
+import { SelectModule } from 'primeng/select';
 
 @Component({
   selector: 'app-new-trip',
-  imports: [FormsModule, InputTextModule, AccordionModule, Button],
+  imports: [FormsModule, InputTextModule, AccordionModule, Button, DatePickerModule, SelectModule],
   templateUrl: './new-trip.html',
   styleUrl: './new-trip.css',
 })
