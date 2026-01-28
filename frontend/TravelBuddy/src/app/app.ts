6c7a31bcfc28c14dd035fc7db329c3a6cd10ac63
Heaji Lee <helenheajilee@gmail.com>
2026-01-28 01:42:38 +0000

    format files

--- frontend/TravelBuddy/src/app/app.ts
+++ frontend/TravelBuddy/src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from "./components/navbar/navbar";
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, Navbar],
+    templateUrl: './app.html',
+    styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('TravelBuddy');
+    protected readonly title = signal('TravelBuddy');
 }
