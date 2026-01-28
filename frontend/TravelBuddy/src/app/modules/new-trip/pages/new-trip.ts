452bf669b17d2e2100dab7e7d57bee2084f51707
Heaji Lee <helenheajilee@gmail.com>
2026-01-28 15:58:13 +0000

    created accordion for new trip page

--- frontend/TravelBuddy/src/app/modules/new-trip/pages/new-trip.ts
+++ frontend/TravelBuddy/src/app/modules/new-trip/pages/new-trip.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { InputTextModule } from 'primeng/inputtext';
+import { AccordionModule } from 'primeng/accordion';
+import { Button } from "primeng/button";
 
 @Component({
   selector: 'app-new-trip',
-  imports: [],
+  imports: [FormsModule, InputTextModule, AccordionModule, Button],
   templateUrl: './new-trip.html',
   styleUrl: './new-trip.css',
 })
