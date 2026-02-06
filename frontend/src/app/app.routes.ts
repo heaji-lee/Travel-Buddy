897ad67cbd4333f90839b15d900b310982fa0c1b
Heaji Lee <helenheajilee@gmail.com>
2026-02-06 01:37:49 +0000

    made nav bar vertical & made the list view as home page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,12 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './modules/home/pages/home.component';
 import { TripsControlComponent } from './modules/trips/pages/trips-control/trips-control.component';
 import { TripsListComponent } from './modules/trips/pages/trips-list/trips-list.component';
 import { ManageComponent } from './modules/manage/pages/manage.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'home', component: HomeComponent },
+    { path: '', component: TripsListComponent },
+    { path: 'home', component: TripsListComponent },
     { path: 'trips/new', component: TripsControlComponent },
     { path: 'trips/:id', component: TripsControlComponent },
-    { path: 'trips', component: TripsListComponent },
     { path: 'manage', component: ManageComponent },
 ];
