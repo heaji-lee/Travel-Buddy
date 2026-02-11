a45ca65f1d571b016380dced82cf328f9ae6f12c
Heaji Lee <helenheajilee@gmail.com>
2026-02-11 15:55:23 +0000

    added weather component

---
+++ frontend/src/app/modules/weather/pages/weather.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather.component',
+  imports: [],
+  templateUrl: './weather.component.html',
+  styleUrl: './weather.component.css',
+})
+export class WeatherComponent {
+
+}
