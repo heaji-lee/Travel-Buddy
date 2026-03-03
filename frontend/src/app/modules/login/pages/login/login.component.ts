095326e9d996f62c7f8f6b9ca00158376796a7c9
Heaji Lee <helenheajilee@gmail.com>
2026-03-03 19:24:19 +0000

    WIP making login dialog

--- frontend/src/app/modules/login/pages/login/login.component.ts
+++ frontend/src/app/modules/login/pages/login/login.component.ts
@@ -2,13 +2,18 @@ import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { DividerModule } from 'primeng/divider';
 import { InputTextModule } from 'primeng/inputtext';
+import { DialogModule } from "primeng/dialog";
 
 @Component({
   selector: 'app-login.component',
-  imports: [ButtonModule, DividerModule, InputTextModule],
+  imports: [ButtonModule, DividerModule, InputTextModule, DialogModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+    isLoginDialogVisible = false;
 
+    showLoginDialog(){
+        this.isLoginDialogVisible = true;
+    }
 }
