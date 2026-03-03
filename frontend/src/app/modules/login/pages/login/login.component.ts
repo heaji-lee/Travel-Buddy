98f46bf4a35e692f5915e12e689c3cf8806fa0c5
Heaji Lee <helenheajilee@gmail.com>
2026-03-03 16:35:03 +0000

    initial login page

--- frontend/src/app/modules/login/pages/login/login.component.ts
+++ frontend/src/app/modules/login/pages/login/login.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login.component',
-  imports: [],
+  imports: [ButtonModule, DividerModule, InputTextModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
