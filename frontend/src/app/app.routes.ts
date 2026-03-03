551762d58677b0798b8c4753d8cdf990e2a86ccd
Heaji Lee <helenheajilee@gmail.com>
2026-03-03 12:53:38 +0000

    Added login component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { TripsListComponent } from './modules/trips/pages/trips-list/trips-list.component';
 import { ManageComponent } from './modules/manage/pages/manage.component';
+import { LoginComponent } from './modules/login/pages/login/login.component';
 
 export const routes: Routes = [
-    { path: '', component: TripsListComponent },
-    { path: 'home', component: TripsListComponent },
-    { path: 'manage', component: ManageComponent },
+    { path: 'login', component: LoginComponent },
+    {
+        path: '',
+        children: [
+            { path: '', component: TripsListComponent },
+            { path: 'home', component: TripsListComponent },
+            { path: 'manage', component: ManageComponent },
+        ],
+    },
+    { path: '**', redirectTo: '' }
 ];
