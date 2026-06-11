0db57d1adc34c87f2c9283b38c36532790e66647
Heaji Lee <helenheajilee@gmail.com>
2026-06-11 15:10:11 +0100

    changed styles

--- frontend/src/app/components/toast/toast.component.ts
+++ frontend/src/app/components/toast/toast.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { ToastModule } from 'primeng/toast';
-import { Tag } from 'primeng/tag';
 
 @Component({
     selector: 'app-toast',
-    imports: [CommonModule, ButtonModule, ToastModule, Tag],
+    imports: [CommonModule, ButtonModule, ToastModule],
     templateUrl: './toast.component.html',
     styleUrl: './toast.component.css',
 })
